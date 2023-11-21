890e503b5d70b8edd315d8fd5f2ffc6879b75811
Lucia Duenas <marialuciaduenas@gmail.com>
2023-11-21 13:47:33 -0300

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import { useState } from 'react'
 import './App.css'
-import {HomeScreen} from './Pages/home/home_screen'
 import { StoreScreen } from './Pages/store/store_screen'
 import { AboutScreen } from './Pages/about/about_screen'
 import { ContactScreen } from './Pages/contact/contact_screen'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HomeScreen } from './Pages/home/home_screen'
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<HomeScreen/>}/>
+        <Route path="/" element={<HomeScreen/>} />
         <Route path='/about' element={<AboutScreen/>}/>
         <Route path='/store' element={<StoreScreen/>}/>
         <Route path='/contact' element={<ContactScreen/>}/>
